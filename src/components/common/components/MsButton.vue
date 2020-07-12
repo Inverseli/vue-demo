d0abca70083709fb73d56dadf6cc5e6a341f468f
inverseli <1920727772@qq.com>
2020-07-12 14:56:16 +0800

    my button

---
+++ src/components/common/components/MsButton.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <div>
+    <el-button v-for="(btn, index) in buttons"
+               :key="index"
+               @click="click(btn)"
+               :size="btn.size"
+               :type="btn.type"
+               :icon="btn.icon"
+    >
+      {{btn.tip}}
+    </el-button>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "MsButton",
+    props: {
+      buttons: Array
+    },
+    methods: {
+      click(btn) {
+        if (btn.exec instanceof Function) {
+          btn.exec();
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
